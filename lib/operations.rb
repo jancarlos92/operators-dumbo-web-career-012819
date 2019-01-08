76a8f3ac1b6ab6e931c927781c35da53e753ffb8
JanCarlos Acevedo <jancarlos92@me.com>
2019-01-08 05:34:10 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -6,7 +6,7 @@ def unsafe?(speed)
     "safe"
   end
     
-def not_safe?(speed)
-  speed > 60 && speed < 40 ? "not safe" : "safe"
-end
+##def not_safe?(speed)
+##  speed > 60 && speed < 40 ? "not safe" : "safe"
+##end
 end
